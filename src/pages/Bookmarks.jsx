37b48f0f58f1d2a9775ce5cd1976cd37fb127211
lokeshloki3 <lokesh1992p@gmail.com>
2024-11-22 16:15:41 +0530

    ui updated

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -39,14 +39,15 @@ const Bookmarks = () => {
   };
 
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <div className="w-3/4 text-center">
-        <h2 className="text-2xl font-bold mb-4">Your Bookmarks</h2>
+    <div>
+      <div className="w-[90%] mx-auto">
+        <h2 className="text-2xl text-center font-bold mb-4">Your Bookmarks</h2>
+        <p className="text-lg text-center font-semibold mb-4">Click to see current Weather</p>
 
         {bookmarks.length > 0 ? (
-          <div>
+          <div className="grid grid-cols-4 gap-4 ml-6 mr-6">
             {bookmarks.map((bookmark, index) => (
-              <div key={index} className="mb-4 p-4 border-b border-gray-300">
+              <div key={index} className="mb-4 p-4 border border-blue-200 rounded-xl text-center">
                 <Link
                   to="/cities"
                   className="text-blue-500 hover:underline"
@@ -63,7 +64,7 @@ const Bookmarks = () => {
                   onClick={() =>
                     handleRemoveBookmark(bookmark.latitude, bookmark.longitude)
                   }
-                  className="text-red-500 hover:text-red-700 mt-2"
+                  className="text-red-500 hover:text-red-700 mt-2 hover:underline"
                 >
                   Remove Bookmark
                 </button>
--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -1,13 +1,22 @@
 // Weather.js
-import React, { useContext, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { MyContext } from '../MyContext';
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { MyContext } from "../MyContext";
 
 const Weather = () => {
   const navigate = useNavigate();
   const [city, setCity] = useState("");
   const [cities, setCities] = useState([]);
-  const { text, setText, latitude, setLatitude, longitude, setLongitude, country, setCountry } = useContext(MyContext);
+  const {
+    text,
+    setText,
+    latitude,
+    setLatitude,
+    longitude,
+    setLongitude,
+    country,
+    setCountry,
+  } = useContext(MyContext);
 
   // Get the list of bookmarks from localStorage
   const [bookmarks, setBookmarks] = useState([]);
@@ -85,20 +94,29 @@ const Weather = () => {
         </div>
 
         {bookmarks.length > 0 && (
-          <div className="mt-5">
-            <h2 className="text-xl font-semibold mb-2">Your Bookmarks</h2>
-            {bookmarks.map((bookmark, index) => (
-              <div
-                key={index}
-                className="cursor-pointer mb-2 p-2 border-b hover:bg-gray-100"
-                onClick={() => handleBookmarkClick(bookmark)}
+          <div className="mt-32">
+            <h2 className="text-xl font-semibold mb-8">Your Bookmarks</h2>
+            <div className="grid grid-cols-4 gap-4">
+              {bookmarks.map((bookmark, index) => (
+                <div
+                  key={index}
+                  className="cursor-pointer mb-2 p-2 border border-blue-200 rounded-xl"
+                  onClick={() => handleBookmarkClick(bookmark)}
+                >
+                  {bookmark.city}, {bookmark.country}
+                </div>
+              ))}
+            </div>
+            <div className="mt-4">
+              <Link
+                to="/bookmarks"
+                className="border border-blue-200 p-2 hover:bg-blue-500 w-96 mx-auto rounded-lg font-semibold cursor-pointer"
               >
-                {bookmark.city}, {bookmark.country}
-              </div>
-            ))}
+                Click here to see more than 10 bookmarks
+              </Link>
+            </div>
           </div>
         )}
-
       </div>
     </div>
   );
